9e5d919aff344113297d90b903f9662c6e6f28fa
Cong Le <longchike@gmail.com>
2025-06-28 00:10:54 -0700

    initial setup

---
+++ Shock_Absorber/Shock_AbsorberApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shock_AbsorberApp.swift
+//  Shock_Absorber
+//
+//  Created by Cong Le on 6/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Shock_AbsorberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
